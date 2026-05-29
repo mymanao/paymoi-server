72fc13986cdbe51197afbdb7af380afc1302f181
tinarskii <tinvv@outlook.co.th>
2026-05-30 04:56:13 +0700

    feat: add testnet

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,8 +1,11 @@
-import {AlchemyProvider, ethers} from "ethers";
+import {ethers} from "ethers";
 import abi from "./abi.json";
 
-const baseAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-const provider = new ethers.AlchemyProvider("base", process.env.API_KEY);
+const baseAddress = process.env.NODE_ENV === "production"
+    ? "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+    : "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
+const network = process.env.NODE_ENV === "production" ? "base" : "base-sepolia";
+const provider = new ethers.AlchemyProvider(network, process.env.API_KEY);
 export const contracts = new ethers.Contract(baseAddress, abi, provider);
 
 export let decimals = await contracts.decimals!()
