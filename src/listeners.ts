044bb67798fcfef204356b1d5dc9a88ea80314a6
tinarskii <tinvv@outlook.co.th>
2026-05-30 12:29:07 +0700

    fix: remove all listeners before listening

--- src/listeners.ts
+++ src/listeners.ts
@@ -2,6 +2,7 @@ import {type BigNumberish, ContractEventPayload, formatUnits} from "ethers";
 import {contracts, decimals} from "./contracts.ts";
 
 export async function startListeners(walletSocket: Map<string, any>, cb: (from: string, to: string, amount: string, txhash: string) => void) {
+    contracts.removeAllListeners();
     contracts.on(contracts.filters.Transfer!(null, null),
         (from: string, to: string, value: BigNumberish, event: ContractEventPayload) => {
             if (!walletSocket.has(to.toLowerCase())) return;
